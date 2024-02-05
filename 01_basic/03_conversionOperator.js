2acf810c5c330f0aa97cf56e29e03bb18bd1cd98
Mithun Chide <61077157+MithunChide@users.noreply.github.com>
2024-02-05 19:02:17 +0000

    conversion operator

---
+++ 01_basic/03_conversionOperator.js
@@ -0,0 +1,38 @@
+ let score = "33";
+
+ console.log(typeof score); //number
+ console.log(typeof (score));
+
+ let valueInNumber = Number(score);
+ console.log(typeof (valueInNumber)); // number
+ console.log(valueInNumber); // 100
+
+ // "33" => 33
+ // "33abs" => NaN
+ // true => 1, false => 0
+
+ let isLoggedIn = 1;
+
+ let booleanIsLoggedIn = Boolean (isLoggedIn);
+ console.log(booleanIsLoggedIn); // true
+ console.log(typeof (booleanIsLoggedIn)); // Boolean
+
+ // 1 => true, 0 => false
+ // "" => false
+//  "mithun" => true
+
+let someNumber = 209;
+
+let stringNumber = String (someNumber)
+console.log(stringNumber); // 209
+console.log(typeof (stringNumber)); // string
+
+
+
+
+
+ 
+
+
+
+
